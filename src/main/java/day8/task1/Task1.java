4425683bf6dd644ed87b496223bea0275811db33
Kanishe <a.kanishevskii@gmail.com>
2021-10-08 15:29:05 +0300

    added day8 task1

---
+++ src/main/java/day8/task1/Task1.java
@@ -0,0 +1,27 @@
+package day8.task1;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        String numbers = "";
+        long before = System.currentTimeMillis();
+        for (int i = 0; i <= 2000; i++) {
+            System.out.println();
+            numbers += i + " ";
+        }
+        long afrer = System.currentTimeMillis();
+        System.out.println("Millis " + (afrer - before));
+        System.out.println(numbers);
+
+        StringBuilder stringBuilder = new StringBuilder("");
+        long before2 = System.currentTimeMillis();
+
+        for (int i = 0; i <= 2000; i++)
+            stringBuilder.append(i).append(" ");
+        long afrer2 = System.currentTimeMillis();
+
+        System.out.println("Millis " + (afrer2 - before2));
+        System.out.println(stringBuilder);
+
+    }
+}
